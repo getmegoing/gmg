09ff4f3fa839356faf203edbb570f009ef4600d1
Korey Hinton <korey.hinton@gmail.com>
2016-10-09 20:46:16 -0600

    Update install.sh

--- eclipse/install.sh
+++ eclipse/install.sh
@@ -1,2 +1,3 @@
 tce-load openjdk-7-jre.tcz
 wget "http://mirror.cc.columbia.edu/pub/software/eclipse/technology/epp/downloads/release/neon/1/eclipse-jee-neon-1-linux-gtk-x86_64.tar.gz"
+tar xvf eclipse-jee-neon-1-linux-gtk-x86_64.tar.gz
