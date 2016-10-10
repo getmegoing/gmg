163d85b3b07d6c3aee61cb556001f16070b00e31
Korey Hinton <korey.hinton@gmail.com>
2016-10-10 16:36:31 -0600

    Update install.sh

--- eclipse/install.sh
+++ eclipse/install.sh
@@ -1,4 +1,4 @@
-tce-load openjdk-7-jre.tcz
+tce-load -wi openjdk-7-jre
 wget "http://download.springsource.com/release/ECLIPSE/mars/2/eclipse-java-mars-2-linux-gtk.tar.gz"
 tar xvf eclipse-java-mars-2-linux-gtk.tar.gz
 cd eclipse/
