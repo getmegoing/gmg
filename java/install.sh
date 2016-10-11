7ee87a82d2ccec01975c138ada643f411fc7642b
Korey Hinton <korey.hinton@gmail.com>
2016-10-10 19:58:52 -0600

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -1,6 +1,6 @@
-# tce-load -wi openjdk-7-jre
-wget http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-i586.tar.gz
-tar xvf jdk-8u101-linux-i586.tar.gz
-tce-load -wi java-installer
+tce-load -wi openjdk-7-jre
+#wget http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-i586.tar.gz
+#tar xvf jdk-8u101-linux-i586.tar.gz
+#tce-load -wi java-installer
 echo "gmg java install...done!"
 echo "Reboot required to use java command"
