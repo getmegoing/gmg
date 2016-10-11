49cc83048fbb39303f2a4cf5401cdf308f9af0c7
Korey Hinton <korey.hinton@gmail.com>
2016-10-10 19:55:14 -0600

    Update install.sh

--- eclipse/install.sh
+++ eclipse/install.sh
@@ -1,6 +1,7 @@
+tce-load chromium-browser
 wget "http://download.springsource.com/release/ECLIPSE/mars/2/eclipse-java-mars-2-linux-gtk.tar.gz"
 tar xvf eclipse-java-mars-2-linux-gtk.tar.gz
 cd eclipse/
 chmod +x eclipse
-./eclipse -repository "https://dist.springsource.com/release/TOOLS/gradle/nightly"
+#./eclipse -repository "https://dist.springsource.com/release/TOOLS/gradle/nightly"
 echo "gmg eclipse install...done"
